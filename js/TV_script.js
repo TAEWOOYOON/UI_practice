b8bbfcc3703ab46af7d22972e56c53ecd15b961f
TAEWOOYOON <102507688+TAEWOOYOON@users.noreply.github.com>
2022-09-15 13:21:47 +0900

    Add files via upload

--- js/TV_script.js
+++ js/TV_script.js
@@ -1,21 +1,66 @@
 const buttonMovie = document.querySelector('#button_movie');
+const buttonDrama = document.querySelector('#button_drama');
+const buttonEnt = document.querySelector('#button_ent');
+const buttonThe = document.querySelector('#button_the');
 
 const resultMovie = document.querySelector('.result_movie');
+const resultDrama = document.querySelector('.result_drama');
+const resultEnt = document.querySelector('.result_ent');
+const resultThe = document.querySelector('.result_the');
 
 
 
-const button = document.querySelector('.bt');
+const button_movie = document.querySelector('#button_movie');
+const button_drama = document.querySelector('#button_drama');
+const button_ent = document.querySelector('#button_ent');
+const button_the = document.querySelector('#button_the');
 
 function view_movie(){
     if (resultMovie.style.display == 'none') {
-        button.style.backgroundColor = 'firebrick';
+        button_movie.style.backgroundColor = 'firebrick';
         resultMovie.style.display = 'flex';
     }
     else{ 
         resultMovie.style.display = 'none';
-        button.style.backgroundColor = '#dcb088';
+        button_movie.style.backgroundColor = '#dcb088';
+    }
+}
+
+function view_drama(){
+    if (resultDrama.style.display == 'none') {
+        button_drama.style.backgroundColor = 'firebrick';
+        resultDrama.style.display = 'flex';
+    }
+    else{ 
+        resultDrama.style.display = 'none';
+        button_drama.style.backgroundColor = '#dcb088';
+    }
+}
+
+function view_ent(){
+    if (resultEnt.style.display == 'none') {
+        button_ent.style.backgroundColor = 'firebrick';
+        resultEnt.style.display = 'flex';
+    }
+    else{ 
+        resultEnt.style.display = 'none';
+        button_ent.style.backgroundColor = '#dcb088';
+    }
+}
+
+function view_the(){
+    if (resultThe.style.display == 'none') {
+        button_the.style.backgroundColor = 'firebrick';
+        resultThe.style.display = 'flex';
+    }
+    else{ 
+        resultThe.style.display = 'none';
+        button_the.style.backgroundColor = '#dcb088';
     }
 }
 
 
 buttonMovie.addEventListener('click', view_movie)
+buttonDrama.addEventListener('click', view_drama)
+buttonEnt.addEventListener('click', view_ent)
+buttonThe.addEventListener('click', view_the)
